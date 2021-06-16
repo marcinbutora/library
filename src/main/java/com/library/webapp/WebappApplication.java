20b25a4045055bb261cff368d43a7297543206e7
mb83pl <Doctype1983!>
2021-06-16 21:15:09 +0200

    CorsRegistry removed

--- src/main/java/com/library/webapp/WebappApplication.java
+++ src/main/java/com/library/webapp/WebappApplication.java
@@ -10,9 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -31,20 +28,6 @@ public class WebappApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(WebappApplication.class, args);
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                WebMvcConfigurer.super.addCorsMappings(registry);
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("PUT", "DELETE", "GET", "POST")
-                        .allowedHeaders("*");
-            }
-        };
     }
 
     public void run(String... args) throws Exception {
