d2860c07bbfe0914d0f09b0ab22ef2df002bb8b9
mb83pl <Doctype1983!>
2021-06-16 21:46:20 +0200

    added remotemysql.com as primary database, removed commandlinerunner

--- src/main/java/com/library/webapp/WebappApplication.java
+++ src/main/java/com/library/webapp/WebappApplication.java
@@ -1,56 +1,13 @@
 package com.library.webapp;
 
-import com.library.webapp.model.Book;
-import com.library.webapp.model.Person;
-import com.library.webapp.model.Rental;
-import com.library.webapp.repository.BookRepository;
-import com.library.webapp.repository.PersonRepository;
-import com.library.webapp.repository.RentalRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @SpringBootApplication
-public class WebappApplication implements CommandLineRunner {
-
-    @Autowired
-    private PersonRepository personRepository;
-
-    @Autowired
-    private BookRepository bookRepository;
-
-    @Autowired
-    private RentalRepository rentalRepository;
+public class WebappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebappApplication.class, args);
     }
-
-    public void run(String... args) throws Exception {
-        Book duchy = new Book("Duchy", "Książka o duchach", "1234-5678");
-        Book kosmici = new Book("Kosmici", "Książka o kosmitach", "2345-6789");
-        Book titanic = new Book("Titanic", "O statku", "125383838");
-        bookRepository.saveAll(List.of(duchy, kosmici, titanic));
-
-        Person person1 = new Person("Stanisław", "Kowalski", "Międzyrzecze Górne", LocalDateTime.of(2021, 10, 21, 10, 20, 00));
-        Person person2 = new Person("Aleksandra", "Nowicka", "Zalesie", LocalDateTime.of(2021, 10, 22, 15, 22, 45));
-        Person ania = new Person("Anna", "Butora", "Twardorzeczka", LocalDateTime.of(2021, 6, 9, 12, 15, 22));
-        Person Zuzia = new Person("Zuzanna", "Butora", "Żywiec", LocalDateTime.of(2016, 1, 6, 21, 30, 00));
-        personRepository.saveAll(List.of(person1, person2, ania, Zuzia));
-
-        Rental rental1 = new Rental(duchy, person1, LocalDateTime.of(2021, 8, 16, 19, 22, 00));
-        Rental rental2 = new Rental(kosmici, person2, LocalDateTime.of(2021, 8, 17, 20, 15, 00));
-        Rental rental3 = new Rental(duchy, person2, LocalDateTime.now());
-        Rental rentalania = new Rental(duchy, ania, LocalDateTime.now());
-        Rental zuziakosmici = new Rental(kosmici, Zuzia, LocalDateTime.now());
-        Rental zuziaduchy = new Rental(duchy, Zuzia, LocalDateTime.now());
-        Rental stanislawtitanic = new Rental(titanic, person1, LocalDateTime.now());
-        rentalRepository.saveAll(List.of(rental1, rental2, rental3, rentalania, zuziakosmici, zuziaduchy, stanislawtitanic));
-    }
-
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,1 @@
-insert into book values(1, 'test 1', 'test desc 1', 'test isbn 1');
-insert into person values(1, 'Maciej', 'Butora', 'Żywiec', '2021-10-09 23:45');
-insert into person values(2, 'Kacper', 'Butora', 'Żywiec', '2021-10-09 23:45');
-insert into person values(3, 'Zuzanna', 'Butora', 'Żywiec', '2021-10-09 23:45');
-insert into person values(4, 'Anna', 'Butora', 'Żywiec', '2021-10-09 23:45');
+
