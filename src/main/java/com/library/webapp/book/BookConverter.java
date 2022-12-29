bc4e032012c3008abf8472286407d461ca52749d
Marcin Butora <36995841+marcinbutora@users.noreply.github.com>
2022-12-29 15:20:01 +0100

    BookConverter fixed

--- src/main/java/com/library/webapp/book/BookConverter.java
+++ src/main/java/com/library/webapp/book/BookConverter.java
@@ -3,20 +3,10 @@ package com.library.webapp.book;
 public class BookConverter {
     public static BookDTO entityToDTO(Book book) {
         BookDTO bookDTO = new BookDTO();
-        bookDTO.setTitle(book.getTitle());
-        bookDTO.setDescription(book.getDescription());
-        bookDTO.setIsbn(book.getIsbn());
-        bookDTO.setBookcover(book.getBookCover());
+        bookDTO.setTitle(book.title());
+        bookDTO.setDescription(book.description());
+        bookDTO.setIsbn(book.isbn());
+        bookDTO.setBookcover(book.bookCover());
         return bookDTO;
     }
-
-    public static Book dtoToEntity(BookDTO bookDTO) {
-        Book book = new Book();
-        book.setTitle(bookDTO.getTitle());
-        book.setDescription(bookDTO.getDescription());
-        book.setIsbn(bookDTO.getIsbn());
-        book.setBookCover(bookDTO.getBookcover());
-        return book;
-    }
-
 }
